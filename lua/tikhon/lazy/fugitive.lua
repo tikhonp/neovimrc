da8ac934582e2a1f710c002464bf03385196eded
TikhonP <tikhon.petrishchev@gmail.com>
2024-10-10 15:59:29 +0300

    Added GIGAPUSH

--- lua/tikhon/lazy/fugitive.lua
+++ lua/tikhon/lazy/fugitive.lua
@@ -17,11 +17,25 @@ return {
 
                 local bufnr = vim.api.nvim_get_current_buf()
                 local opts = { buffer = bufnr, remap = false }
+
                 vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git('gigapush')
+                end, opts)
+
+                vim.keymap.set("n", "<leader>P", function()
                     vim.cmd.Git('push')
+                end, opts)
+
+                vim.keymap.set("n", "<leader>h", function()
+                    vim.cmd.Git('hub')
+                end, opts)
+
+                vim.keymap.set("n", "<leader>l", function()
+                    vim.cmd.Git('lab')
                 end, opts)
             end,
         })
     end,
 
 }
+
