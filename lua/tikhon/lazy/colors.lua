85daf49b02e999b9973f7dd7d8115c21ff61f726
TikhonP <tikhon.petrishchev@gmail.com>
2024-10-18 13:59:16 +0300

    Removed disabled italics for coloscheme

--- lua/tikhon/lazy/colors.lua
+++ lua/tikhon/lazy/colors.lua
@@ -9,13 +9,13 @@ return {
     config = function()
         require('rose-pine').setup({
             styles = {
-                italic = false,
                 transparency = true,
             },
         })
 
         vim.cmd.colorscheme("rose-pine")
 
+        -- make background transparent to make it always black
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
