022754b127a02a9269019b5fbc2750286bac06cc
TikhonP <tikhon.petrishchev@gmail.com>
2024-11-30 23:16:21 +0300

    Added remap option for harpoon to alt-123...8

--- lua/tikhon/lazy/harpoon.lua
+++ lua/tikhon/lazy/harpoon.lua
@@ -1,10 +1,10 @@
 return {
 
-    'ThePrimeagen/harpoon',
+    "ThePrimeagen/harpoon",
 
-    branch = 'harpoon2',
+    branch = "harpoon2",
 
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { "nvim-lua/plenary.nvim" },
 
     config = function()
         local harpoon = require("harpoon")
@@ -20,11 +20,10 @@ return {
         }
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts) end)
 
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+        for i = 1, 8 do
+            vim.keymap.set("n", string.format("<space>%d", i), function() harpoon:list():select(i) end)
+            vim.keymap.set("n", string.format("<A-%d>", i), function() harpoon:list():select(i) end)
+        end
     end,
 
 }
