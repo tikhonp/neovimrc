aafe98c0de7aa1a36b2d49d58f908c3b97aebd07
TikhonP <tikhon.petrishchev@gmail.com>
2026-03-14 21:24:50 +0300

    Fixed config for latest treesitter

--- lua/custom-plugins/treesitter.lua
+++ lua/custom-plugins/treesitter.lua
@@ -18,19 +18,15 @@ return {
     },
 
     config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-
         vim.filetype.add({ extension = { templ = "templ" } })
 
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
+        require('nvim-treesitter.parsers').templ = {
             install_info = {
                 url = "https://github.com/vrischmann/tree-sitter-templ.git",
                 files = { "src/parser.c", "src/scanner.c" },
                 branch = "master",
             },
         }
-
         vim.treesitter.language.register("templ", "templ")
 
         vim.opt.foldtext = 'v:lua.vim.treesitter.foldtext()'
