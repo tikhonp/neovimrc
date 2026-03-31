517a6446f8fe26e0d079b78f5d96a3a7210d921f
TikhonP <tikhon.petrishchev@gmail.com>
2026-03-31 19:14:55 +0300

    Updated typst conf

--- lua/custom-plugins/typst.lua
+++ lua/custom-plugins/typst.lua
@@ -4,9 +4,10 @@ return {
     version = '1.*',
     keys = {
         { "<leader>tp", "<cmd>TypstPreview<CR>", "n" },
+        { "<leader>tt", "<cmd>TypstPreviewSyncCursor<CR>", "n"}
     },
     opts = {
         open_cmd = 'google-chrome-stable --app=%s',
-        invert_colors = 'auto',
+        -- invert_colors = 'auto',
     },
 }
